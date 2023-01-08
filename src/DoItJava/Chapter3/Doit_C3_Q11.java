3aac1f0c22c6e52a74b954404eaa1ecba5434d35
O_jeegu <lumirite0@gmail.com>
2023-01-09 00:33:47 +0900

    fix: Add file Doit_C3_Q11.java
    -파일명 : Doit_C3_Q11.java
    -작성일자 : 2023.01.08
    -문제 : 두잇 알고리즘(자바) 챕터3 문제 11번 (백준 1874)
    -키워드 : Stack, StringBuffer
    -후기 : 문제를 이해하는 데 좀 오래걸렸다. 문제를 푸는 법도 중요하지만 그 전에 빠르게 파악하는 습관을 들여야겠다. Stack의 기본적인 성질을 알아야 풀 수 있다. for, if, while 등 반복문과 조건문의 순서를 잘 생각해보아야 한다. String과 StringBuffer의 차이점과 StringBuffer와 Print의 차이점을 알아야 한다.
    -수정사항 : 주석 설명을 stack 성질과 연관되는 내용으로 수정

--- src/DoItJava/Chapter3/Doit_C3_Q11.java
+++ src/DoItJava/Chapter3/Doit_C3_Q11.java
@@ -11,32 +11,32 @@ public class Doit_C3_Q11 { //스택으로 오름차순 수열 만들기
         StringBuffer bf = new StringBuffer(new StringBuffer()); //출력객체
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n]; //수열의 개수 입력
-        for(int i = 0; i < n; i++){ //수열을 이루는 정수 입력
+        for(int i = 0; i < n; i++){ //stack을 통해 출력하고 싶은 임의의 수열 순서 입력
             arr[i] = Integer.parseInt(br.readLine());
         }
         Stack<Integer> stack = new Stack<>();
-        int num = 1; //수열의 값과 비교할 숫자. 수열의 크기가 1보다 같거나 크므로 1로 초기화
-        boolean flag = true; //수열 출력 여부를 저장하는 변수
+        int num = 1; //숫자 1~n까지 오름차순으로 push 해야하므로 1로 초기화
+        boolean flag = true; //stack이 num과 arr의 순서에 맞게 후입선출 되는지 확인하는 변수(후입선출 실패시 false)
         for(int i = 0; i < n; i++) {
-            if(arr[i] >= num) {
+            if(arr[i] >= num) { // arr[i]와 같아질 때까지 num을 오름차순으로 push
                 while(arr[i] >= num){
                     stack.push(num++); // stack.push(num)과 num++을 한줄로 표현
                     bf.append("+\n"); //push 했으므로 '+'
                 } //arr[i] < num이 되는 순간 while문 종료
-                stack.pop(); //stack top에 arr[i] = num의 값이 저장된 상태에서 pop
+                stack.pop(); //arr[i] = num인 수를 pop
                 bf.append("-\n"); //pop 했으므로 '-'
             }
             else if(arr[i] < num) {
-                if(arr[i] == stack.pop()) { //stack top 값이 arr[i]와 같으면 그대로 출력
+                if(arr[i] == stack.pop()) { //arr[i]가 num 보다 작고 stack.top과 같은 경우 후입선출 조건을 만족하므로 pop
                     bf.append("-\n");
                 }
-                else { //같지 않은 경우 stack을 비울 수 없으므로 no를 출력
+                else { //같지 않은 경우 후입선출 조건에 어긋나므로 flag 값을 false로 바꾸고 no 출력
                     System.out.println("NO");
-                    flag = false; //오름차순 출력이 불가능하므로 flag 값을 false로 바꿈
+                    flag = false;
                     break;
                 }
             }
         }
-        if(flag) System.out.print(bf); //만약 문자열로 출력하고 싶으면 System.out.print(bf.toString()); 처리
+        if(flag) System.out.print(bf); //만약 문자열로 출력하고 싶으면 System.out.print(bf.toString());
     }
 }
