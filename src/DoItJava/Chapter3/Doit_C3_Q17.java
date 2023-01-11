026bb0b74d416f75ada880372589124a2a71e482
O_jeegu <lumirite0@gmail.com>
2023-01-12 00:04:07 +0900

    feat: Add file Doit_C3_Q17.java
    -파일명 : Doit_C3_Q17.java
    -작성일자 : 2023.01.11
    -문제 : 두잇 알고리즘(자바) 챕터3 문제 17번 (백준 1427)
    -키워드 : 선택정렬

---
+++ src/DoItJava/Chapter3/Doit_C3_Q17.java
@@ -0,0 +1,28 @@
+package DoItJava.Chapter3;
+
+import java.util.Scanner;
+
+public class Doit_C3_Q17 { //소트인사이드(선택정렬)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int[] arr = new int[str.length()];
+        for(int i = 0; i < str.length(); i++) {
+            arr[i] = Integer.parseInt(str.substring(i, i + 1));
+        }
+        for(int i = 0; i < str.length(); i++) {
+            int max = i; //배열의 맨 앞에 있는 인덱스를 최대값으로 초기화
+            for(int j = i+1; j<str.length(); j++) {
+                if(arr[j] > arr[max]) max = j; //arr 배열을 돌며 최대값이 들어있는 인덱스를 찾음
+            }
+            if(arr[i] < arr[max]) {
+                int tmp = arr[i];
+                arr[i] = arr[max];
+                arr[max] = tmp;
+            }
+        }
+        for(int x : arr) {
+            System.out.print(x);
+        }
+    }
+}
