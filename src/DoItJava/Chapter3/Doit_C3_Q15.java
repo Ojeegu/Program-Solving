1d2066b916f271930e132eee006a8e707f611466
O_jeegu <lumirite0@gmail.com>
2023-01-11 23:59:20 +0900

    feat: Add file Doit_C3_Q15.java
    -파일명 : Doit_C3_Q15.java
    -작성일자 : 2023.01.11
    -문제 : 두잇 알고리즘(자바) 챕터3 문제 15번 (백준 2750)
    -키워드 : 버블정렬

---
+++ src/DoItJava/Chapter3/Doit_C3_Q15.java
@@ -0,0 +1,28 @@
+package DoItJava.Chapter3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Doit_C3_Q15 { //수 정렬하기1(버블정렬)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        for(int i = 0; i < N-1; i++) {
+            for(int j = 0; j < N-1-i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        for(int x : arr) {
+            System.out.println(x);
+        }
+    }
+}
