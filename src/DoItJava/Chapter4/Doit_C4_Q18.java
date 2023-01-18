66cb593e616348b2917dca907d2917b5c5211a0a
O_jeegu <lumirite0@gmail.com>
2023-01-18 11:26:29 +0900

    feat: Add file Doit_C4_Q18.java
    -파일명 : Doit_C4_Q18.java
    -작성일자 : 2023.01.18
    -문제 : 두잇 알고리즘(자바) 챕터4 문제 18번 (백준 11399)
    -키워드 : 정렬
    -후기 : Arrays.sort를 사용하면 비교적 쉬운 문제. 만약 시간 제한이 짧다면 Arrays.sort를 사용하지 못하므로 다른 방법으로도 풀어보는게 좋을 것 같다.

---
+++ src/DoItJava/Chapter4/Doit_C4_Q18.java
@@ -0,0 +1,30 @@
+package DoItJava.Chapter4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Doit_C4_Q18 { //ATM 인출 시간 계산하기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine()); //사람 수 입력
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine()); //사람이 각각 돈 인출하는데 필요한 시간 입력
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr); //arr정렬 오름차순
+
+        int totalTime = 0; //모든 사람 걸리는 시간 (합계)
+        int perTime = 0; //사람 당 걸리는 시간
+        for(int i = 0; i < n; i++){
+            perTime += arr[i];
+            totalTime += perTime;
+        }
+        System.out.println(totalTime);
+    }
+}
