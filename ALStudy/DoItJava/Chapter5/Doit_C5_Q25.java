023028ede1a4345b5f1fbd86fc1213c29b612a21
O_jeegu <lumirite0@gmail.com>
2023-01-21 02:06:09 +0900

    feat : Add file Doit_C5_Q25.java
    -파일명 : Doit_C5_Q25.java
    -작성일자 : 2023.01.21
    -문제 : 두잇 알고리즘(자바) 챕터5 문제 25번 (백준 13023)
    -키워드 : DFS
    -후기 : for문과 if문의 위치, 그리고 visited를 검증하는 위치가 헷갈렷다. 비슷한 문제(23번)를 풀었을 때와 비슷한 부분에서 계속 헷갈려서 이 부분을 더 명확히 짚고 넘어가는게 좋겠다.

---
+++ ALStudy/DoItJava/Chapter5/Doit_C5_Q25.java
@@ -0,0 +1,52 @@
+package DoItJava.Chapter5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Doit_C5_Q25 { //친구 관계 파악하기
+    static ArrayList<ArrayList<Integer>> list;
+    static boolean[] visited;
+    static boolean isValid;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        list = new ArrayList<>();
+        visited = new boolean[n]; //방문 정점 체크용 배열
+        isValid = false; //DFS가 5번 이상 들어갔는지 체크하는 변수
+        for(int i = 0; i < n; i++) { //list에 n개의 arryaList 객체 생성
+            list.add(new ArrayList<>());
+        }
+        for(int i = 0; i < m; i++) { //양방향 노드 추가
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            list.get(u).add(v);
+            list.get(v).add(u);
+        }
+        for(int i = 0; i < n; i++) {
+            DFS(i, 1);
+            if(isValid) break;
+        }
+        if(isValid) System.out.println(1);
+        else System.out.println(0);
+    }
+
+    static void DFS(int v, int count) {
+        if (count == 5 || isValid) {
+            isValid = true;
+            return;
+        }
+        visited[v] = true;
+        for (int u : list.get(v)) {
+            if(!visited[u]) { //u가 이미 방문한 곳이라면 count가 늘어나지 않음
+                DFS(u, count + 1);
+            }
+        }
+        visited[v] = false;
+    }
+}
