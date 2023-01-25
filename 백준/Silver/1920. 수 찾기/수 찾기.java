705c4ff4b641b2f2828d91a316a49898239b65a3
dO_the_Jeegu <107015624+Ojeegu@users.noreply.github.com>
2023-01-25 15:11:41 +0900

    [Silver IV] Title: 수 찾기, Time: 700 ms, Memory: 43860 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,48 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-public class Main { //수 찾기
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        int n = Integer.parseInt(br.readLine()); //정수 개수 입력
-        int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken()); //n개의 정수를 배열에 저장
-        }
-        Arrays.sort(arr); //arr배열을 오름차순 정렬
-        int m = Integer.parseInt(br.readLine()); //찾을 정수 개수 입력
-        int[] findArr = new int[m];
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < m; i++) {
-            findArr[i] = Integer.parseInt(st.nextToken()); //m개의 정수를 배열에 저장
-        }
-        for(int i = 0; i < m; i++) { //arr 배열에 찾는 정수가 있는지 없는지 확인
-            boolean flag = false;
-            int target = findArr[i]; //찾아야 하는 수
-            int start = 0;
-            int end = n-1;
-            while(start <= end) {
-                int mid = (start + end) / 2;
-                if(arr[mid] < target) {
-                    start = mid + 1;
-                }
-                else if(arr[mid] > target) {
-                    end = mid - 1;
-                }
-                else {
-                    flag = true;
-                    break;
-                }
-            }
-            if(flag) sb.append(1 + "\n");
-            else sb.append(0 + "\n");
-        }
-        System.out.println(sb);
-    }
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		int[] numArr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			numArr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(numArr);
+		int m = Integer.parseInt(br.readLine());
+		int[] findArr = new int[m];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < m; i++) {
+			findArr[i] = Integer.parseInt(st.nextToken());
+		}
+		for(int i = 0; i < m; i++) {
+			int target = findArr[i];
+			int start = 0;
+			int end = n - 1;
+			boolean flag = false;
+			while(start <= end) {
+				int mid = (start + end) / 2;
+				if(numArr[mid] < target) start = mid + 1;
+				else if(numArr[mid] > target) end = mid - 1;
+				else {
+					flag = true;
+					break;
+				}
+			}
+			if(flag) sb.append(1 + "\n");
+			else sb.append(0 + "\n");
+		}
+		System.out.println(sb);
+	}
 }
