bb81bfccf04a25f18efda17649d79bb9322f950a
dO_the_Jeegu <107015624+Ojeegu@users.noreply.github.com>
2023-02-07 22:56:53 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 188 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -1,34 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         long min = Long.parseLong(st.nextToken());
         long max = Long.parseLong(st.nextToken());
-
-        boolean[] checkArr = new boolean[(int)(max-min) +1];
-        long sqrtMax = (long)Math.sqrt(max);
-
+        int sqrtMax = (int)Math.sqrt(max);
+        boolean[] checkArr = new boolean[(int)(max - min) + 1];
         for(long i = 2; i <= sqrtMax; i++) {
-            long pow = i * i;
-            long start = 0;
-            if(min % pow != 0) start = pow - (min % pow);
-
+            long pow = i*i;
+            long start = pow - (min % pow);
+            if(min % pow == 0) start = 0;
             for(long j = start; j < checkArr.length; j += pow) {
                 checkArr[(int)j] = true;
             }
         }
-
-        int count = 0;
+        int	count = 0;
         for(int i = 0; i < checkArr.length; i++) {
             if(!checkArr[i]) count++;
         }
-
         System.out.println(count);
     }
 }
